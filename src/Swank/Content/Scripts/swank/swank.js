08e13847fc407b37aae8cacb577cf3c24bf0c070
mob <mob@mikeobrien.net>
2012-10-11 22:35:39 -0400

    Finished the UI.

--- src/Swank/Content/Scripts/swank/swank.js
+++ src/Swank/Content/Scripts/swank/swank.js
@@ -77,16 +77,22 @@
                 $(content).find(".json").hide();
                 return false;
             });
-            
-            //$('.copy-code').click(function () {
-            //    console.log(window.clipboardData);
-            //    if (window.clipboardData) {
-            //        var code = $(this).closest("table").find('.json:visible, .xml:visible')
-            //            .map(function (x) { return x.text(); }).join("\r\n");
-            //        window.clipboardData.setData("Text", code);
-            //    }
-            //    return false;
-            //});
+
+            $(window).resize(function () { $('.code-raw-container').remove(); });
+
+            $('td.code').click(function () {
+                var table = $(this).closest("table");
+                var code = table.find('.code.json:visible, .code.xml:visible').map(function () { return $(this).text(); }).get().join("\r\n");
+                var top = table.find('.code:visible:first');
+                var bottom = table.find('.code:visible:last');
+                var container = $('<div class="code-raw-container" style="' +
+                    'top: ' + (top.offset().top + 1) + 'px;' +
+                    'left: ' + (top.offset().left + 1) + 'px;' +
+                    'width: ' + (top.outerWidth() - 2) + 'px;' +
+                    'height: ' + ((bottom.offset().top - top.offset().top) + bottom.outerHeight() - 2) + 'px;' +
+                    '"><textarea class="code-raw">' + code + '</textarea></div>').appendTo(content);
+                $(container).find('textarea').select().blur(function (x) { $(x.target).parent().remove(); });
+            });
         } else {
             content.html('No module our resource specified.');
         }
